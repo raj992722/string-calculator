37ed15d38bde9546a24894668d7e5527cbb38557
Rohit raj <raj992722@gmail.com>
2025-04-18 17:56:54 +0530

    failed case for two comma-separated strings numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -16,5 +16,10 @@ describe("StringCalculator", ()=>{
     test("add: should return the number itself for a single number string",()=>{
         expect(calculator.add('1')).toBe(1);
         expect(calculator.add('5')).toBe(5);
+    });
+
+    test("add: should return the sum for two comma-separated numbers",()=>{
+        expect(calculator.add('1,5')).toBe(6);
+        expect(calculator.add("10,2")).toBe(12);
     })
 })
