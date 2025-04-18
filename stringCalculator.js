38690b1ef5b0749c39a5b9956e482ca0888e2f52
Rohit raj <raj992722@gmail.com>
2025-04-18 18:03:35 +0530

    using reduce for summation

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,10 +5,14 @@ class StringCalculator {
             return 0;
         }
         const numberArray = numbers.split(',');
-        let sum=0;
-        for(const numStr of numberArray){
-            sum += parseInt(numStr,10);
-        }
+        // let sum=0;
+        // for(const numStr of numberArray){
+        //     sum += parseInt(numStr,10);
+        // }
+        // using reduce for summation
+        return numberArray.reduce((sum,numStr)=>{
+            return sum+parseInt(numStr,10);
+        },0)
 
 
         return sum;
