1e0e30b0b313745928575dd9d5bd6f159f6896f9
Rohit raj <raj992722@gmail.com>
2025-04-18 17:51:58 +0530

    one number failed case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,5 +11,10 @@ describe("StringCalculator", ()=>{
 
     test("add: should return 0 for an empty string", ()=>{
         expect(calculator.add("")).toBe(0);
+    });
+
+    test("add: should return the number itself for a single number string",()=>{
+        expect(calculator.add('1')).toBe(1);
+        expect(calculator.add('5')).toBe(5);
     })
 })
