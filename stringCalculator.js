945223b539ac901d5a9591433173ea0e847511ee
Rohit raj <raj992722@gmail.com>
2025-04-18 18:00:11 +0530

    success case for two comma-separated string numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,8 +4,14 @@ class StringCalculator {
         if(numbers === ""){
             return 0;
         }
+        const numberArray = numbers.split(',');
+        let sum=0;
+        for(const numStr of numberArray){
+            sum += parseInt(numStr,10);
+        }
 
-        return parseInt(numbers,10);
+
+        return sum;
     }
 }
 
