c9fb6b791a223e46d734f2e7d7198ce0082386a4
Rohit raj <raj992722@gmail.com>
2025-04-18 18:18:14 +0530

    passed case for newLines

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,14 +4,21 @@ class StringCalculator {
         if(numbers === ""){
             return 0;
         }
-        const numberArray = numbers.split(',');
+        // default delimiterRegex
+        let delimiterRegex = /[,\n]/;
+        const numberArray = numbers.split(delimiterRegex);
         // let sum=0;
         // for(const numStr of numberArray){
         //     sum += parseInt(numStr,10);
         // }
         // using reduce for summation
         return numberArray.reduce((sum,numStr)=>{
-            return sum + parseInt(numStr,10);
+            const trimmedStr = numStr.trim();
+            // skip potential empty strings resulting from split (e.g., "1, \n2")
+            if(trimmedStr === "") return sum;
+            const num = parseInt(trimmedStr,10);
+
+            return sum + (isNaN(num) ? 0 : num);
         },0)
 
 
